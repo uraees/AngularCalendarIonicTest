5e7f7f6f8f69b547dd806e3c0db8806e9d4beba1
Raees Uzhunnan <raeez@hotmail.com>
2017-06-14 13:05:08 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { CalendarModule } from 'angular-calendar';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+      CalendarModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CalendarMonthViewDay, CalendarDateFormatter } from 'angular-calendar'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  today: Date;
+  days_label: Array<string> = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+
+  selectedDay: CalendarMonthViewDay;
+  calendarView = 'month';
+  viewDate = new Date();
+  vm = this;
+  events = [
+    {
+      title: 'An event',
+      color: "#5b8257",
+      startsAt: new Date("06/20/2017"),
+      endsAt: new Date("06/20/2017"),
+      draggable: true,
+      resizable: true,
+      actions: "actions"
+    }
+  ];
+  cellIsOpen = true;
 
   constructor(public navCtrl: NavController) {
+    this.today = new Date();
+  };
 
+    createHtml = function (htmlStr)  {
+    var frag = document.createDocumentFragment(),
+      temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+      frag.appendChild(temp.firstChild);
+    }
+    return frag;
   }
+
+
+
+  dayModifier = (day:CalendarMonthViewDay) => {
+   // alert( "day.inMonth" + day.inMonth + " events"+ day.events+ " backgroundColor"+day.backgroundColor+ " badgeTotal"+day.badgeTotal);
+   day.cssClass = 'cal-day-selected' ;
+   //alert( JSON.stringify(day) );
+  };
+
+  dayClicked = (day, cell) => {
+    //alert("Hello! I am an alert box!!" + Object.prototype.toString.call(day));
+    alert(JSON.stringify(day));
+    if (this.selectedDay) {
+      delete this.selectedDay.cssClass;
+    }
+    day.cssClass = 'cal-day-selected';
+    this.selectedDay = day;
+  }
+
 
 }
